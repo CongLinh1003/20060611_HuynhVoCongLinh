f740dca764d79a82c5da7853b151a1ffa5f0f68a
CongLinh1003 <01633007559mc@gmail.com>
2023-09-17 22:17:29 +0700

    "adding 5-7ex"

--- js1.js
+++ js1.js
@@ -40,7 +40,7 @@ const avgScore = arrayScore => {
 }
 
 const validTest = (arrayScore) => {
-   return arrayScore.some((value) => value > 100)
+    return arrayScore.some((value) => value > 100)
 }
 
 const whoWin = (arrayA, arrayB) => {
@@ -60,11 +60,55 @@ whoWin(Dolphins, Koalas)
 //Coding Challenge #4
 
 const tips = billValue => {
-    return billValue>50 && billValue<=300? billValue*0.15 : billValue*0.2
+    return billValue > 50 && billValue <= 300 ? billValue * 0.15 : billValue < 50 ? 0 : billValue * 0.2
 }
 
 const tipResult = billValue => {
     console.log(`The bill was ${billValue}, the tip was ${tips(billValue)}, and the total value ${billValue + tips(billValue)}`);
 }
 
-tipResult(275)
+tipResult(450)
+
+//Coding Challenge #5
+const calcAverage = inputAr => {
+    const avg = (inputAr.reduce((acc, curVal) => acc + curVal, 0))
+    return avg / inputAr.length
+}
+
+const checkWinner = (arrA, arrB) => {
+    let a = calcAverage(arrA)
+    let b = calcAverage(arrB)
+    return a > b ? console.log(`Dolhins win ( ${a} vs. ${b})`) : console.log(`Koalas win ( ${b} vs ${a})`)
+}
+
+Dolphins = [44, 23, 71]
+Koalas = [65, 54, 49]
+
+checkWinner(Dolphins, Koalas)
+
+//Coding Challenge #6
+const tinhTips = billValue => {
+    return billValue > 50 && billValue <= 300 ? billValue * 0.15 : billValue < 50 ? 0 : billValue * 0.2
+}
+const calcTips = billValue => {
+    let t = billValue > 50 && billValue <= 300 ? billValue * 0.15 : billValue < 50 ? 0 : billValue * 0.2
+
+    console.log(`The bill was ${billValue}, the tip was ${t}, and the total value ${billValue + t}`);
+}
+bills = [125,555,44]
+totalBills = []
+bills.forEach(value => {
+    calcTips(value)
+    totalBills.push(`${tinhTips(value)+ value}`)
+});
+console.log("The TotalArray: "+totalBills);
+
+//Coding Challenge #6
+
+const person = {
+    {
+        fullname: 'Mark Miller',
+        'height':  1.78,
+        'mass': 70
+    }
+}
